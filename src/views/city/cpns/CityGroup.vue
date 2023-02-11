bfe14bb8481d48081f6717b506683d9e5380a888
qq2391833769 <9739735+qq2391833769@user.noreply.gitee.com>
2023-02-11 18:16:24 +0800

    day3

---
+++ src/views/city/cpns/CityGroup.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="city_hot">
+    <div class="hot_title">热门</div>
+    <ul class="hot_list">
+      <li 
+        v-for="city in cityGroupData.hotCities"
+        @click="setCity(city)"
+        >
+        <span>
+          {{ city.cityName }}
+        </span>
+      </li>
+    </ul>
+  </div>
+  <div class="city_group">
+    <van-index-bar :index-list="indexList">
+      <div v-for="cities in cityGroupData?.cities" >
+        <van-index-anchor :index="cities.group" />
+        <div v-for="city in cities.cities" @click="setCity(city)">
+          <van-cell :title="city.cityName" />
+        </div>
+      </div>
+    </van-index-bar>
+  </div>
+</template>
+
+
+<script setup>
+  import { defineProps, ref, watch, watchEffect } from 'vue';
+  import useCityStore from '@/store/modules/city';
+  import { useRouter } from 'vue-router';
+
+  const cityStore = useCityStore();
+  const router = useRouter();
+
+  const indexList = ref([])
+
+  //接收默认值
+  const props = defineProps({
+    cityGroupData: {
+      type: Object,
+      default: {}
+    }
+  });
+
+  //监听数据变化
+  watch(() => props.cityGroupData, () => {
+    console.log('检测')
+    indexList.value = props.cityGroupData.cities?.map(item => {
+      console.log(item.group)
+      return item.group;
+    });
+  });
+
+  //设置当前城市并返回home页面
+  const setCity = (city) => {
+    cityStore.setCurrentCity(city);
+    router.push('/home');
+  }
+  
+</script>
+
+
+<style lang='less' scoped >
+
+  .hot_title {
+    margin-bottom: 10px;
+  }
+  .city_hot {
+    padding: 0 10px;
+    font-size: 14px;
+
+    ul {
+      display: flex;
+      // justify-content: space-around;
+      flex-wrap: wrap;
+
+      li {
+        flex:0 0 25%;
+        display: flex;
+        box-sizing: border-box;
+        justify-content: center;
+        align-items: center;
+        margin: 10px 0px;
+
+        span {
+          padding: 10px 10px;
+          width: 50px;
+          background-color: #fbf1e9;
+          border-radius: 20px;
+          text-align: center;
+        }
+      }
+    }
+  }
+  .city_group {
+    // --van-index-anchor-background: '#f7f8fa'
+    --van-index-anchor-background: #f7f8fa !important;
+  }
+
+</style>
---
+++ src/views/home/cpns/HomeHeader.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="home_header">
+      <span>弘源旅途</span>
+    </div>
+    <div class="home_img">
+      <img src="@/assets/img/home/banner.webp" alt="">
+    </div>
+    <div class="home_location">
+      <div class="location_city">{{ cityStore.currCity.cityName }}</div>
+      <div class="location_mark" @click="locationClick">
+        <span>我的位置</span>
+        <img src="@/assets/img/home/icon_location.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+  import useCityStore from '@/store/modules/city';
+  import { useRouter } from 'vue-router';
+
+  //获取城市store
+  const cityStore = useCityStore();
+
+  //获取router对象
+  const router = useRouter();
+
+  //地址点击事件
+  const locationClick = () => {
+    router.push('/city')
+  }
+
+</script>
+
+
+<style lang='less' scoped >
+  .home_header {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 50px;
+      color: #eb9c69;
+      font-weight: 700;
+
+  }
+
+    .home_img {
+
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+    .home_location {
+      display: flex;
+      justify-content: space-between;
+      font-size: 14px;
+      padding: 20px 0px;
+
+      .location_city {
+        display: flex;
+        align-items: center;
+        margin-left: 15px;
+      }
+      .location_mark {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 80px;
+        margin-right: 15px;
+
+        img {
+          width: 20px;
+          height: 20px;
+        }
+      }
+    }
+</style>
